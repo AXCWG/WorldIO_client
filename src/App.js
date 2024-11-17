85eed140a09534443f3e07cb01b01faf2b2733d0
AXCWG <xiey0@qq.com>
2024-11-18 03:28:05 +0800

    WOOOW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+var ws = new WebSocket("wss://andyxie.cn:8181");
+
+function send(username, msg) {
+    ws.send(JSON.stringify({username: username, msg: msg}));
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    ws.onmessage = (e) => {
+        setMsgBuffer(msgBuffer + e.data + "\n")
+        document.getElementById("output").scrollTop =  document.getElementById("output").scrollHeight + 40;
+    }
+
+    const [msgBuffer, setMsgBuffer] = useState("Welcome to WorldIO!\n");
+    const [msg, setMsg] = useState("");
+    const [username, setUsername] = useState("Anonymous");
+    return (
+        <div className="App" style={{width: "100%", display: 'flex', flexDirection: 'column', height: '100%'}}>
+            <div style={{flex: 2, width: '99.8%'}}>
+                <textarea id={"output"} style={{width: "100%", height: "100%" ,  resize: "none",}}
+                          disabled={true} value={msgBuffer}></textarea>
+            </div>
+            <div style={{display: "flex", flexDirection: "row", height: "30px", width: "100%"}}>
+                <input placeholder={"Stuff"} style={{padding: "0", resize: "none", flexGrow: 1}} value={msg}
+                       onChange={(s) => {
+                           setMsg(s.target.value)
+                       }} onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        send(username, msg);
+                        setMsg("")
+                    }
+                }}></input>
+                <input placeholder={"Username"}  value={username} onChange={(e) => {
+                    setUsername(e.target.value)
+                }}></input>
+                <button onClick={() => {
+                    send(username, msg)
+                    setMsg("")
+                }}>Send
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
